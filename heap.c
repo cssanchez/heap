a3c551cfa402901ec89c3e727495a6f6c14e7f47
cssanchez <sanchezcristiansebastian@gmail.com>
2016-05-26 18:24:32 -0300

    Add files via upload

--- heap.c
+++ heap.c
@@ -18,29 +18,30 @@ struct heap {
 	cmp_func_t cmp;
 };
 
+
 // Auxiliares.
 
 void swap(void **vector, size_t posicion_a, size_t posicion_b) {
 	void* aux_a = vector[posicion_a];
 	vector[posicion_a] = vector[posicion_b];
 	vector[posicion_b] = aux_a;
-	printf("por aca pasa..SWAP\n");
 }
 
 
 void upheap(heap_t *heap, size_t posicion) {
 	if (posicion == 0) return;
 	size_t posicion_padre = (posicion - 1) / 2;
-	/*printf("\n");
-	printf ("POSICION: %ld\n", posicion);
-	printf ("PISICION PADRE: %ld\n", posicion_padre);
-	printf("\n");*/
+	printf ("POSICION: %zu\n", posicion);
+	printf ("PISICION PADRE: %zu\n", posicion_padre);
 	if (heap->cmp(heap->vector[posicion], heap->vector[posicion_padre]) > 0) {
 		swap(heap->vector, posicion, posicion_padre);
 		upheap(heap, posicion_padre);
 	}
-	return;
 }
+
+int i = 4;
+int *p = &i;
+
 
 size_t max(void** vector, cmp_func_t cmp, size_t posicion_a, size_t posicion_b) {
 	if (!posicion_a) return posicion_b;
@@ -50,11 +51,10 @@ size_t max(void** vector, cmp_func_t cmp, size_t posicion_a, size_t posicion_b) 
 
 
 void downheap(void **vector, size_t tam_vector, cmp_func_t cmp, size_t posicion) {
-	printf("por aca pasa...DOWNHEAP\n");	
 	size_t pos_hijo_izq = (2 * posicion) + 1;
 	size_t pos_hijo_der = (2 * posicion) + 2;
-	if (pos_hijo_izq >= tam_vector - 1) pos_hijo_izq = false;
-	if (pos_hijo_der >= tam_vector - 1) pos_hijo_der = false;
+	if (pos_hijo_izq >= tam_vector) pos_hijo_izq = false;
+	if (pos_hijo_der >= tam_vector) pos_hijo_der = false;
 	if (!pos_hijo_izq && !pos_hijo_der) return;
 	size_t pos_hijo_mayor = max(vector, cmp, pos_hijo_izq, pos_hijo_der);
 	if (cmp(vector[posicion], vector[pos_hijo_mayor]) < 0) {
@@ -63,24 +63,16 @@ void downheap(void **vector, size_t tam_vector, cmp_func_t cmp, size_t posicion)
 	}
 }
 
-
 void heapify(void*vector[], cmp_func_t cmp, size_t cantidad){
-		printf("por aca pasa..\n");
-		size_t i = cantidad/2 -1;
+		size_t i = (cantidad/2) -1;
+		printf("\n%zu\n", i);
 	while(i > 0){
-		downheap(vector, cantidad,cmp,i);
+		downheap(vector, cantidad, cmp, i);
 		i--;
 	}
-	downheap(vector,cantidad,cmp,i);
-	printf("por aca pasa..\n");
-}
+	downheap(vector, cantidad, cmp, i);
+	printf("%zu\n", i);
 
-bool aux_redimensionar(heap_t* heap, size_t tam_nuevo){
-	void* aux = realloc(heap->vector, tam_nuevo);
-	if (!aux) return false;
-	heap->vector = aux;
-	heap->tam = tam_nuevo;
-	return true;
 }
 
 // Primitivas.
@@ -99,6 +91,29 @@ heap_t *heap_crear(cmp_func_t cmp) {
 	return heap;
 }
 
+
+/*heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp) {
+	heap_t *heap = malloc(sizeof(heap_t));
+	if (!heap) return NULL;
+	heap->vector = heapify(arreglo, n, cmp); // Modificar segpun como quede heapify
+	heap->cmp = cmp;
+	heap->cantidad = n;
+	heap->tam = n * 2;
+	return heap;
+}*/
+
+
+void heap_destruir(heap_t *heap, void destruir_elemento(void *e)){
+	if (destruir_elemento) {
+		for (int i = 0; i < heap->cantidad; ++i) {
+			destruir_elemento(heap->vector[i]);
+		}
+	}
+	free(heap->vector);
+	free(heap);
+}
+
+
 bool heap_encolar(heap_t *heap, void *elem) {
 	if (heap_esta_vacio(heap)) {
 		heap->vector[0] = elem;
@@ -108,29 +123,26 @@ bool heap_encolar(heap_t *heap, void *elem) {
 	heap->vector[heap->cantidad] = elem;
 	upheap(heap, heap->cantidad);
 	heap->cantidad++;
-	/*for (int i = 0; i < heap->cantidad; i++){
-		printf("%i / ", *(int*) heap->vector[i]);
+	for (int i = 0; i < heap->cantidad; ++i)
+	{
+		printf("%i /", *(int*)heap->vector[i]);
 	}
-	printf("\n");*/
-	if (heap->cantidad == heap->tam){ 
-		aux_redimensionar(heap, FACTOR_REDIMENSION * heap->tam); 
-	}
+	printf("\n");
 	return true;
 }
+
 
 void *heap_desencolar(heap_t *heap) {
 	if (heap_esta_vacio(heap)) return NULL;
 	void *elem_a_devolver = heap->vector[0];
 	heap->vector[0] = heap->vector[heap->cantidad - 1];
-	downheap(heap->vector, 0,heap->cmp, heap->cantidad);
+	downheap(heap->vector, heap->cantidad, heap->cmp, 0);
 	heap->cantidad--;
-	for (int i = 0; i < heap->cantidad; ++i){
+	for (int i = 0; i < heap->cantidad; ++i)
+	{
 		printf("%i /", *(int*)heap->vector[i]);
 	}
 	printf("\n");
-	if (heap->cantidad <= heap->tam / DIVISOR_ACHIQUE){
-		aux_redimensionar(heap, heap->tam / FACTOR_REDIMENSION);
-	}
 	return elem_a_devolver;
 }
 
@@ -140,10 +152,12 @@ void *heap_ver_max(const heap_t *heap){
 	return heap->vector[0];
 }
 
+
 bool heap_esta_vacio(const heap_t *heap){
 	if (heap->cantidad == 0) return true;
 	return false;
 }
+
 
 size_t heap_cantidad(const heap_t *heap){
 	return heap->cantidad;
